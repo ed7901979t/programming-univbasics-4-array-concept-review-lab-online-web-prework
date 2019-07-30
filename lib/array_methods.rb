c2002d5863d43c3283b9c26808591f4024ddd8ff
clean-command-1851 <ed7901979t@gmail.com>
2019-07-30 10:12:42 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,31 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  length=array.length
+  length.times do |value_to_find|
+    puts array.|value_to_find|
 end
+numbers=[1,2,3,4,5]
+find_element_index(numbers, [4])
 
 def find_max_value(array)
-  # Add your solution here
+  length=array.length
+  if
+  length.times > length.times - 1   
+    max=length.times
+    puts #{max} 
+  else 
+    puts nil
+  end
 end
+find_max_value(numbers)
 
 def find_min_value(array)
-  # Add your solution here
+  length=array.length
+  if
+  length.times < length.times - 1 
+    min=length.times
+    puts #{min} 
+  else 
+    puts nil
+  end
 end
+find_min_value(numbers)
